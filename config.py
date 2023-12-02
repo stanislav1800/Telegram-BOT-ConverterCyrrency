fb3bb0911d391202ad3f5dcb83fad11dc151dd8d
Станислав <77958472+stanislav1800@users.noreply.github.com>
2023-12-02 21:20:29 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+
+TOKEN = "6760779459:AAEauHOb6WZwCIrcC6IpqiUwt750MfBP4YY"
+
+keys = {'рубль': 'RUB', 'доллар': 'USD', 'евро': 'EUR'}
---
+++ extensions.py
@@ -0,0 +1,31 @@
+import requests
+import json
+from config import keys
+
+class ConvertionExeption(Exception):
+    pass
+
+class CyrrencyConverter:
+    @staticmethod
+    def convert(quote = str, base = str, amount = str):
+        if quote == base:
+            raise ConvertionExeption(f'Неудалось перевести одинаковые валюты {base}')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionExeption(f'Неудалось обработать валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionExeption(f'Неудалось обработать валюту {base}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionExeption(f'Yеправильно введено число {amount}')
+
+        r = requests.get(f'https://v6.exchangerate-api.com/v6/deb9e429e53c193552d668fd/pair/{quote_ticker}/{base_ticker}/{amount}')
+        total_base = json.loads(r.content)
+        return round(total_base["conversion_result"], 2)
